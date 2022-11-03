03c99553d95c47fc9fc77d2c92aeef6754a78437
Vicki Yang <vwyangdev@gmail.com>
2022-11-03 19:21:26 -0400

    refactoring and cleanup

--- graph.py
+++ graph.py
@@ -1,69 +1,36 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-DAYS = 365
-ROWS = 20
-COLS = 20
-
-curr_row = 1
-curr_col = 1
-row_nums = []
-col_nums = []
-for day in range(1, DAYS + 1):
-    if day % ROWS == 0:
-        row_nums.append(curr_row)
-        curr_row += 1
-        col_nums.append(curr_col)
-        curr_col = 1  # reset col num
-    else:
-        row_nums.append(curr_row)
-        col_nums.append(curr_col)
-        curr_col += 1
-
-# print(f"rows: {row_nums} ***** {len(row_nums)}")
-# print(f"cols: {col_nums} ***** {len(col_nums)}")
-
-# Data cleaning
 df = pd.read_csv('data/nyr_2022_sample.csv')
+
+# Split date column
 df[['Month', 'Day', 'Year']] = df['Date'].str.split('/', expand=True)
-# print(df['Month'])
 df['Month'] = df['Month'].astype(int)
 df['Day'] = df['Day'].astype(int)
+df['Year'] = df['Year'].astype(int)
+
+# Replace falsy values with 0's, otherwise the boolean conversion will return True for "0" and "0.0"
 df.fillna(0, inplace=True)
 df = df.replace('0', 0)
 df = df.replace('0.0', 0)
 
-df['Row Num'] = row_nums
-df['Col Num'] = col_nums
-df['Resolutions Met'] = sum(
-    [
-        df['Exercise'].astype(bool),
-        df['Floss'].astype(bool),
-        df['Writing'].astype(bool),
-        df['Coding'].astype(bool),
-        df['Skincare'].astype(bool)
-    ]
-)
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# print(df)
+# Create new column that sums up total resolutions met per day
+resolution_bools = df.iloc[:, 1:6].astype(bool)
+df['Resolutions Met'] = resolution_bools.sum(axis=1)
+print(df)
 
-# df_melted = pd.melt(df, id_vars='Row Num', value_vars=['Exercise', 'Floss', 'Writing', 'Coding', 'Skincare'],
-#                     var_name='Resolution', value_name='Value')
-#
-# print(df_melted)
-# df_map = df_melted.pivot("Row Num", "Resolution", "Value")
+# Generate heatmap
 df_map = df.pivot("Month", "Day", "Resolutions Met")
-
-# glue = sns.load_dataset("glue").pivot("Model", "Task", "Score")
-# nyr_map = sns.heatmap(df_map, vmin=0, vmax=5, cmap="inferno", xticklabels=False, yticklabels=False)
 nyr_map = sns.heatmap(df_map, vmin=0, vmax=5, cmap="inferno", square=True, cbar_kws={'orientation': 'horizontal'})
 
-# outline day I started Adderall
-rect = plt.Rectangle([13, 3], 1, 1, color="gold",
-                     linewidth=2.5, fill=False)
-nyr_map.add_patch(rect)
-# nyr_map.set(xlabel=None, ylabel=None)
+# Create rectangular outlines for notable days
+notable_days = {
+    "start_adderall": [14, 4],  # date in [day, month] format
+}
+for event, day in notable_days.items():
+    day_coords = [x - 1 for x in day]
+    rect = plt.Rectangle(day_coords, 1, 1, color="gold", linewidth=2.5, fill=False)
+    nyr_map.add_patch(rect)
+
 plt.show()
