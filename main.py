184686d1106abbf423eb0cebe011ba1fb5bffd4c
Vicki Yang <vwyangdev@gmail.com>
2022-11-22 15:51:52 -0500

    log to dict

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import sys
 from datetime import date
 import json
+import csv
 from utils import *
 
 
-def view_active_resolutions():
-    pass
+def get_active_resolutions():
+    with open("sample.json", "r") as f:
+        all_res_dict = json.load(f)
+    active_res_dict = {}
+    for key, val in all_res_dict.items():
+        if val["is_active"]:
+            active_res_dict[key] = val
+    # print(f"active resolutions: {active_res_dict}")
+    return active_res_dict
 
 
 def add_resolution():
@@ -24,9 +33,9 @@ def add_resolution():
         # Invalid input
         pass
     is_binary = input("Is this resolution's outcome binary? For example, for the resolution to exercise, "
-                            "a binary outcome is whether you exercised or did not exercise. "
-                            "In contrast, a categorical outcome names the kind of exercise you did (e.g. "
-                            "run/bike/swim). (Y/N): ")
+                      "a binary outcome is whether you exercised or did not exercise. "
+                      "In contrast, a categorical outcome names the kind of exercise you did (e.g. "
+                      "run/bike/swim). (Y/N): ")
     if is_binary.upper() == "Y":
         is_binary = True
     elif is_binary.upper() == "N":
@@ -40,10 +49,11 @@ def add_resolution():
     res_dict = {
         res_id: {
             "res_descript": res_descript,
-            "res_creation_date": res_creation_date.strftime("%m-%d-%Y"),
+            "res_creation_date": res_creation_date.strftime("%m/%d/%Y"),
             "is_active": is_active,
-            "res_expiration_date": res_expiration_date.strftime("%m-%d-%Y"),
+            "res_expiration_date": res_expiration_date.strftime("%m/%d/%Y"),
             "is_binary": is_binary,
+            "data": {},
         }
     }
 
@@ -59,7 +69,19 @@ def deactivate_resolution():
 
 
 def log_resolutions():
-    pass
+    active_res = get_active_resolutions()
+    today = date.today().strftime("%m/%d/%Y")
+    for res, val in active_res.items():
+        if val['is_binary']:
+            response = input(f"Did you `{val['res_descript']}` today? (Y/N): ").upper()
+            if response == "Y":
+                val['data'][today] = True
+            elif response == "N":
+                val['data'][today] = False
+            else:
+                print(f"Invalid input: {response}")
+    print(active_res)
+    # Persist to file
 
 
 def visualize_resolutions():
@@ -75,15 +97,23 @@ OPTION_MENU = {
         "text": "add resolution",
         "function": add_resolution,
     },
+    "3": {
+        "text": "view active resolutions",
+        "function": get_active_resolutions,
+    },
 }
 
 
 def main():
-    print(f"Welcome to ✨ Resolve ✨\n")
-    for key, val in OPTION_MENU.items():
-        print(f"{key} - {val['text']}")
-    command = input("\nWhat would you like to do?: ")
-    OPTION_MENU[command]["function"]()
+    while True:
+        print(f"Welcome to ✨ Resolve ✨\n")
+        for key, val in OPTION_MENU.items():
+            print(f"{key} - {val['text']}")
+        print("Enter 'q' to quit\n")
+        command = input("What would you like to do? (enter #): ")
+        if command == "q":
+            sys.exit("Goodbye!")
+        OPTION_MENU[command]["function"]()
 
 
 if __name__ == "__main__":
